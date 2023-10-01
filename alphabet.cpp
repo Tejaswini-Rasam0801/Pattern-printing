019629950db53f63214fa2f154916cb4db061204
Tejaswini Rasam <91883721+Tejaswini-Rasam0801@users.noreply.github.com>
2023-10-01 13:04:30 +0530

    Add files via upload

---
+++ alphabet.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char letter = 'a';            //char letter = 'a' + i-1;
+    for (int i=1; i<=5; i++){
+        for(char letter='a'; letter<='e'; letter++){   //int j= 1; j<=5; j++
+            cout<<letter<<" ";                 //cout<<j<<" ";
+        
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
